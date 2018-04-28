204bd52e7f85f6a95faa6f15f883d481dc12621c
Safisyx <safisyx@gmail.com>
2018-04-29 01:37:40 +0200

    Add some web app contents

---
+++ src/components/Web.js
@@ -0,0 +1,82 @@
+import React,{PureComponent} from 'react'
+import {Grid, Image, Card} from 'semantic-ui-react'
+import './Web.css'
+class Web extends PureComponent{
+  render() {
+    const {vWidth}=this.props
+    let nColumns
+    if (vWidth>=1274 || (vWidth>=768 && vWidth<992)){
+      nColumns=2
+    }
+    if (vWidth<768 || (vWidth>=992 && vWidth<1274)){
+      nColumns=1
+    }
+    return (
+        <Grid columns={nColumns} className='Web' style={{backgroundColor:'rgba(34,36,38,.1)'}}>
+          <Grid.Column>
+            <Card centered raised color='black'>
+              <Image src= '/hangman.png'/>
+              <Card.Content>
+                <Card.Header>
+                  Hangman (React/Redux)
+                </Card.Header>
+                <Card.Meta>
+                  <a className='a' target="_blank" rel="noopener noreferrer" href='https://safisyx-hangman.herokuapp.com/'>Deployed</a><span> on </span>
+                  <a className='b' target="_blank" rel="noopener noreferrer" href='https://www.heroku.com/'>Heroku</a>
+                </Card.Meta>
+                <Card.Description>
+                  Individual assignment at <a className='b' target="_blank" rel="noopener noreferrer" href='https://www.codaisseur.com'>Codaisseur</a>
+                </Card.Description>
+              </Card.Content>
+              <Card.Content textAlign='center'>
+                <p>See code on <a className='a' target="_blank" rel="noopener noreferrer" href='https://github.com/Safisyx/Hangman'>Github</a></p>
+              </Card.Content>
+            </Card>
+          </Grid.Column>
+
+          <Grid.Column>
+            <Card centered raised color='black'>
+              <Image src= '/evaluation.png'/>
+              <Card.Content>
+                <Card.Header>
+                  Student Evaluation Tool
+                </Card.Header>
+                <Card.Description>
+                  Final individual project at <a className='b' target="_blank" rel="noopener noreferrer" href='https://www.codaisseur.com'>Codaisseur</a>.
+                  <br/>I used React/Redux in the frontend and TypeScript/TypeORM in the backend.
+                </Card.Description>
+              </Card.Content>
+              <Card.Content textAlign='center'>
+                <p>See frontend code on <a className='a' target="_blank" rel="noopener noreferrer" href='https://github.com/Safisyx/evaluation-client'>Github</a>
+                <br/>See backend code on <a className='a' target="_blank" rel="noopener noreferrer" href='https://github.com/Safisyx/student-evaluation-api'>Github</a></p>
+              </Card.Content>
+            </Card>
+          </Grid.Column>
+     {
+        /*  <Grid.Column>
+            <Card centered raised color='black'>
+              <Image src= '/hangman.png'/>
+              <Card.Content>
+                <Card.Header>
+                  Hangman (React/Redux)
+                </Card.Header>
+                <Card.Meta>
+                  <a className='a' target="_blank" rel="noopener noreferrer" href='https://safisyx-hangman.herokuapp.com/'>Deployed</a><span> on </span>
+                  <a className='b' target="_blank" rel="noopener noreferrer" href='https://www.heroku.com/'>Heroku</a>
+                </Card.Meta>
+                <Card.Description>
+                  Individual assignment at <a className='b' target="_blank" rel="noopener noreferrer" href='https://www.codaisseur.com'>Codaisseur</a>
+                </Card.Description>
+              </Card.Content>
+              <Card.Content textAlign='center'>
+                <p>See code on <a className='a' target="_blank" rel="noopener noreferrer" href='https://github.com/Safisyx/Hangman'>Github</a></p>
+              </Card.Content>
+            </Card>
+          </Grid.Column>*/}
+
+        </Grid>
+    )
+  }
+}
+
+export default Web
